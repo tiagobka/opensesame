549c9cd4cad48bf57651a6532270cd7e7d1d14f6
tiagobka <tiagobka@msn.com>
2020-04-06 01:58:27 -0700

    On board Logic- Recognizer & setup

--- src/recognizer.py
+++ src/recognizer.py
@@ -3,10 +3,10 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-from setup import SetupRecognizer
+from setup import SetupRecognizer, ControlRecognizer
 
 setup = SetupRecognizer()
-print(setup.cascadeFile)
+control = ControlRecognizer()
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -15,15 +15,39 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 face_cascade  = cv2.CascadeClassifier(setup.cascadeFile)
-print(face_cascade)
 for frame in camera.capture_continuous(rawCapture, format= "bgr", use_video_port=True):
 	image = frame.array
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+	
+	rank = cv2.Laplacian(image, cv2.CV_64F).var()
+	
 	for (x,y,w,h) in faces:
-		cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+		cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 		#roi_gray = gray[y:y+h, x:x+w]
 		#roi_color = image[y:y+h, x:x+w]
+	control.frameCounter = control.frameCounter + 1
+	if (len(faces) > 0 ):
+		control.faceFrameCounter  = control.faceFrameCounter + 1
+		control.trigger()
+	
+	if control.pollImage(setup.pollingTime, rank):
+		control.bestFrame = frame
+	
+	#control.timeout(setup.requestDelay)
+
+	if control.timeUp:
+		delay  = int(time.time()- control.lastSent) > setup.requestDelay
+		stats = control.faceFrameCounter/control.frameCounter*100
+		#print(delay, stats)
+		if (delay & (stats > setup.sensitivity)):
+			print("seding")
+			print(frame)
+			control.lastSent = time.time()
+			
+		control.cleanup()
+		
+           
 	
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1)
--- src/setup.py
+++ src/setup.py
@@ -47,16 +47,57 @@ class SetupRecognizer:
 
 
 
-class controlRecognizer:
+class ControlRecognizer:
 
     def __init__(self):
-        self.nFaces = 0
-        self.bestFrame = None
+        self.bestFrame = None # frame datatype containing best image within a poll time
+        self.bestRank = 0
+        self.startTime = 0 # start of the polltime
+        self.frameCounter = 0 #total frames 
+        self.faceFrameCounter = 0 # number of frames containing a face
+        self.faceDetected = False # Indicates a poll have triggered
+        #self.bestArea = 0  new ranking based on 
+        self.timeUp = False
+        self.lastSent  = 0 #timestamp of last AWS call
+        
+        
+    def trigger(self):
+        if not self.faceDetected:
+            self.startTime = time.time()
+            self.faceDetected = True
+            
+    def pollImage(self, pollTime, rank) -> bool:
+        if self.faceDetected:
+            elapsedTime = int(time.time() - self.startTime)
+            if (elapsedTime > pollTime):
+                self.timeUp = True ## necessary?
+            if rank > self.bestRank:
+                self.bestRank = rank
+                return True
+        return False
+    
+    def timeout(self, delay):
+        if self.timeUp:
+            self.startTime = 0
+            self.faceDetected = False
+            self.bestRank = 0
+            self.frameCounter = 0
+            self.faceFrameCounter = 0
+            self.timeUp = False
+            if (int(time.time()- self.lastSent) > delay):
+                 print("OK To send")
+    def cleanup(self):
         self.startTime = 0
-        self.frameCounter = 0
-        self.faceFramecounter = 0
         self.faceDetected = False
-        self.bestArea = 0
+        self.bestRank = 0
+        self.frameCounter = 0
+        self.faceFrameCounter = 0
+        self.timeUp = False
+        
+            
+                
+        
+        
 
 
 
