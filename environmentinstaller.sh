d98ce71864384197323e853d96e7f084a155662a
tiagobka <tiagobka@msn.com>
2020-04-06 01:51:53 -0700

    BackEnd-on board processing logic

---
+++ environmentinstaller.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake pkg-config -y
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-devy libx264-dev -y
+sudo apt-get install libgtk2.0-dev libgtk-3-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install python3-dev -y
+sudo apt-get install python3-pip -y
+sudo apt-get install libqtgui4 -y
+sudo modprobe bcm2835-v4l2
+sudo apt-get install libqt4-test -y
+sudo apt-get install python3-opencv -y
